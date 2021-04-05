84b74a59ebcc96d25cc7184750f9055d54157c24
Diego Nogare <nogare@outlook.com>
2021-04-05 18:51:24 -0300

    Adicionado novo arquivo

---
+++ models/diabetes/consumo_diabetes.py
@@ -0,0 +1,38 @@
+import urllib.request
+import json
+import os
+import ssl
+
+def allowSelfSignedHttps(allowed):
+    # bypass the server certificate verification on client side
+    if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
+        ssl._create_default_https_context = ssl._create_unverified_context
+
+allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+
+X, y = load_diabetes(return_X_y = True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+data = X_test[0:1].tolist()
+
+body = str.encode(json.dumps(data))
+
+url = 'http://20.201.9.148:80/api/v1/service/diabetes-prod-aks/score'
+api_key = 'iqWSH1MKzIVwtbCxwihZ9FNUmj9SubYH' # Replace this with the API key for the web service
+headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+
+req = urllib.request.Request(url, body, headers)
+
+try:
+    response = urllib.request.urlopen(req)
+
+    result = response.read()
+    print(result)
+except urllib.error.HTTPError as error:
+    print("The request failed with status code: " + str(error.code))
+
+    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+    print(error.info())
+    print(json.loads(error.read().decode("utf8", 'ignore')))
