fb3881b789de0ad1a81c6113aafd4e20c948b2e4
Diego Nogare <nogare@outlook.com>
2021-04-06 10:38:15 -0300

    Criação dos pkl

--- models/diabetes/train.py
+++ models/diabetes/train.py
@@ -25,23 +25,23 @@ data = {"train": {"X": X_train, "y": y_train},
         "test": {"X": X_test, "y": y_test}}
 
 print(X_test)
-#
-# # list of numbers from 0.0 to 1.0 with a 0.05 interval
-# alphas = mylib.get_alphas()
-#
-# for alpha in alphas:
-#     # Use Ridge algorithm to create a regression model
-#     reg = Ridge(alpha=alpha)
-#     reg.fit(data["train"]["X"], data["train"]["y"])
-#
-#     preds = reg.predict(data["test"]["X"])
-#     mse = mean_squared_error(preds, data["test"]["y"])
-#     run.log('alpha', alpha)
-#     run.log('mse', mse)
-#
-#     # Save model in the outputs folder so it automatically get uploaded when running on AML Compute
-#     model_file_name = 'ridge_{0:.2f}.pkl'.format(alpha)
-#     with open(os.path.join('./outputs/', model_file_name), 'wb') as file:
-#         pickle.dump(reg, file)
-#
-#     print('alpha is {0:.2f}, and mse is {1:0.2f}'.format(alpha, mse))
+
+# list of numbers from 0.0 to 1.0 with a 0.05 interval
+alphas = mylib.get_alphas()
+
+for alpha in alphas:
+    # Use Ridge algorithm to create a regression model
+    reg = Ridge(alpha=alpha)
+    reg.fit(data["train"]["X"], data["train"]["y"])
+
+    preds = reg.predict(data["test"]["X"])
+    mse = mean_squared_error(preds, data["test"]["y"])
+    run.log('alpha', alpha)
+    run.log('mse', mse)
+
+    # Save model in the outputs folder so it automatically get uploaded when running on AML Compute
+    model_file_name = 'ridge_{0:.2f}.pkl'.format(alpha)
+    with open(os.path.join('./outputs/', model_file_name), 'wb') as file:
+        pickle.dump(reg, file)
+
+    print('alpha is {0:.2f}, and mse is {1:0.2f}'.format(alpha, mse))
